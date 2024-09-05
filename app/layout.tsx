b7ed0836f8fe19e3279b1e25782c4f11c9e9dff3
Ayan Das <dasayan348@gmail.com>
2024-09-06 02:47:03 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <script
+          id="mcjs"
           dangerouslySetInnerHTML={{
             __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/7ec5a0b77c11407db579e4bfa/7119b6e1d2304fcd4edfffc52.js");`,
           }}
